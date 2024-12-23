ea2e0b6c089066ccc33bb0238431b6713afe1787
Sơn Nguyễn <son@sonng.dev>
2024-12-23 11:40:30 +0000

    feat: add play/pause button

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   const startButton = document.querySelector('button.start-button');
   const grid = document.querySelector('.calendar-grid');
   const music = new Audio('https://res.cloudinary.com/dh7qbp6sy/video/upload/v1734902929/santa-tell-me-ariana-grande_gkiv6o.mp3');
+  const playButton = document.querySelector('button.play-button');
 
   if (!startButton || !grid) {
     return;
@@ -12,4 +13,18 @@ document.addEventListener('DOMContentLoaded', async () => {
     grid.classList.add('calendar-screen');
     music.play();
   });
+
+  playButton.addEventListener('click', () => {
+    if (music.paused) {
+      music.play();
+      playButton.querySelector('.text').textContent = 'Playing';
+      playButton.classList.add('playing');
+      playButton.classList.remove('pausing');
+    } else {
+      music.pause();
+      playButton.querySelector('.text').textContent = 'Pausing';
+      playButton.classList.remove('playing');
+      playButton.classList.add('pausing');
+    }
+  })
 })
