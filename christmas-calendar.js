facbf8ef942ae92272b1dcc1fa85f5d93d5a2f16
Sơn Nguyễn <son@sonng.dev>
2024-12-23 10:52:52 +0000

    feat: add audio and minor styling adjustment

--- christmas-calendar.js
+++ christmas-calendar.js
@@ -1,6 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   const startButton = document.querySelector('button.start-button');
   const grid = document.querySelector('.calendar-grid');
+  const music = new Audio('https://res.cloudinary.com/dh7qbp6sy/video/upload/v1734902929/santa-tell-me-ariana-grande_gkiv6o.mp3');
 
   if (!startButton || !grid) {
     return;
@@ -9,5 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
   startButton.addEventListener('click', () => {
     grid.classList.remove('start-screen');
     grid.classList.add('calendar-screen');
+    music.play();
   });
 })
